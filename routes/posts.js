3e97a2e2413bf95e0ea31e49e3d5b660440574d5
legenddam <techdiamondguru@yahoo.com>
2020-08-17 18:32:22 -0600

    Add commnet Feature

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var multer = require('multer');
+const { post } = require('../app');
 var upload = multer({ dest: './public/images' })
 
   
@@ -16,6 +17,20 @@ router.get('/add', function(req, res, next) {
             return;
         }else{
             res.render('addposts', {title : "Add Posts", categories : categories});
+         }
+    });
+});
+
+router.get('/show/:id', function(req, res, next) {
+    var db = req.db;
+    var posts = db.get('posts');
+   
+    posts.findOne({_id : req.params.id}, function(err, post){
+        if(err){
+            console.log("Posts Database Error");
+            return;
+        }else{
+            res.render('showReadmore', {post : post});
          }
     });
 });
@@ -68,4 +83,58 @@ router.post('/add', upload.single('mainimage'),function(req, res, next) {
             });
     }
 });
+
+router.post('/addcomment', function(req, res, next) {
+
+    var postid = req.body.postid;
+    var name = req.body.name;
+    var email = req.body.email;
+    var body = req.body.body;
+    var comment_date = new Date();
+
+    var db = req.db;
+
+
+    //Form validation
+    req.checkBody('name', 'The Name field is required').notEmpty();
+    req.checkBody('email', 'The Email Filed is requried').notEmpty();
+    req.checkBody('email', 'The Email Filed Format is incorret').isEmail();
+    req.checkBody('body', 'Body Field is required').notEmpty();
+
+    var errors = req.validationErrors();
+    if(errors){
+        var posts = db.get('posts');
+        posts.findOne({_id : postid}, function(err, post){
+            if(err){
+                console.log("Posts Database Error");
+                return;
+            }
+            else{
+                res.render('showReadmore', {"errors": errors, "post" : post});
+            }
+        });
+    }
+    else{
+        var comment = {
+            "name" : name,
+            "email" : email,
+            "body" : body,
+            "commentdate" : comment_date
+        };
+
+        var posts = db.get('posts');
+
+        posts.update({_id : postid}, {$push : {"comments" : comment}},function(err, post){
+            if(err){
+                    res.send(err);
+                }
+                else{
+                    req.flash("success", "The Comments Added");
+                    res.location('/posts/show/'+postid);
+                    res.redirect('/posts/show/'+postid);
+                }
+            });
+    }
+});
+
 module.exports = router;
